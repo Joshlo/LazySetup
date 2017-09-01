4b547c8006d0973b9e7726142219e2836f75f5bb
Kenneth Pedersen <kpedersen@trendsales.com>
2017-09-01 14:42:29 +0200

    first commit

---
+++ LazySetup/Sql/ISqlProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace LazySetup.Sql
+{
+    public interface ISqlProvider
+    {
+        string ConnectionString { get; }
+
+        Task<IEnumerable<TResult>> QueryAsync<TResult>(string sql, object parameters = null);
+
+        Task<IEnumerable<TResult>> QueryAsync<TFirst, TSecond, TResult>(string sql, Func<TFirst, TSecond, TResult> func,
+            string splitOn, object parameters = null);
+        Task<IEnumerable<TResult>> QueryAsync<TFirst, TSecond, TThird, TResult>(string sql, Func<TFirst, TSecond, TThird, TResult> func,
+            string splitOn, object parameters = null);
+        Task<IEnumerable<TResult>> QueryAsync<TFirst, TSecond, TThird, TFourth, TResult>(string sql, Func<TFirst, TSecond, TThird, TFourth, TResult> func,
+            string splitOn, object parameters = null);
+        Task<TResult> FirstOrDefaultAsync<TResult>(string sql, object parameters = null);
+        Task<TResult> QueryMultipleAsync<TResult>(string sql, Func<SqlMapper.GridReader, TResult> func, object parameters = null);
+
+        IEnumerable<TResult> Query<TResult>(string sql, object parameters = null);
+        IEnumerable<TResult> Query<TFirst, TSecond, TResult>(string sql, Func<TFirst, TSecond, TResult> func,
+            string splitOn, object parameters = null);
+        IEnumerable<TResult> Query<TFirst, TSecond, TThird, TResult>(string sql, Func<TFirst, TSecond, TThird, TResult> func,
+            string splitOn, object parameters = null);
+        IEnumerable<TResult> Query<TFirst, TSecond, TThird, TFourth, TResult>(string sql, Func<TFirst, TSecond, TThird, TFourth, TResult> func,
+            string splitOn, object parameters = null);
+        TResult FirstOrDefault<TResult>(string sql, object parameters = null);
+
+        TResult QueryMultiple<TResult>(string sql, Func<SqlMapper.GridReader, TResult> func,
+            object parameters = null);
+    }
+}
---
+++ LazySetup/Sql/SqlSetup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LazySetup.Sql
+{
+    public static class SqlSetup
+    {
+        public static void AddSql(this IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<ISqlProvider>(provider => new SqlProvider(connectionString));
+        }
+    }
+}
