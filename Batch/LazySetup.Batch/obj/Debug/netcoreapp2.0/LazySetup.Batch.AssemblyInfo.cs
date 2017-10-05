efec4b02f13ef6e7277b9516bcf4f9a682c01614
Kenneth Pedersen <kpedersen@trendsales.com>
2017-10-05 11:45:24 +0200

    cleanup

--- Batch/LazySetup.Batch/obj/Debug/netcoreapp2.0/LazySetup.Batch.AssemblyInfo.cs
+++ Batch/LazySetup.Batch/obj/Debug/netcoreapp2.0/LazySetup.Batch.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LazySetup.Batch")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Joshlo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
