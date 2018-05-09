9fc92ddbf4d1d9de4092cebea1e3575d619a52ff
Kenneth Hammer <joshlo@gigaload.dk>
2018-05-09 14:38:17 +0200

    added proxy ips to IP project

--- LazySetup.Ip/IpSetup.cs
+++ LazySetup.Ip/IpSetup.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace LazySetup.Ip
 {
@@ -9,12 +10,14 @@ namespace LazySetup.Ip
     {
         public static IApplicationBuilder UseIpBlocking(this IApplicationBuilder app, IEnumerable<string> ips)
         {
+            app.UseForwardedHeaders();
             app.UseMiddleware<IpBlockingMiddleware>(ips);
             return app;
         }
 
         public static IApplicationBuilder UseIpRestriction(this IApplicationBuilder app, IEnumerable<string> ips)
         {
+            app.UseForwardedHeaders();
             app.UseMiddleware<IpRestrictionMiddleware>(ips);
             return app;
         }
